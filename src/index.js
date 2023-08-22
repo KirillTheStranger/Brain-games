d3f84f0bd0408764585e306f07c7de0ca45d92de
Kirill <faeeeee@gmail.com>
2023-08-22 11:32:02 +0500

    add `!` in losing round

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const gameLogic = (rules, task, rightAnswer) => {
       console.log('Correct!');
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer[i]}'.`);
-      return console.log(`Let's try again, ${userName}`);
+      return console.log(`Let's try again, ${userName}!`);
     }
   }
 
