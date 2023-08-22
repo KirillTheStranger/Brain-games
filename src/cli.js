8900afa455f54817225c43a1628e8c10b94a8607
Kirill <faeeeee@gmail.com>
2023-08-22 11:29:00 +0500

    fix of brain-game issue

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const greetingUser = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May i have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
