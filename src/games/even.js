6fcc8a3a3f485b8b5091b47be72e3833ac8afcf8
Kirill <faeeeee@gmail.com>
2023-08-25 14:52:03 +0500

    change logic of brain-even game

--- src/games/even.js
+++ src/games/even.js
@@ -2,11 +2,19 @@ import gameLogic from '../index.js';
 import { isEven, randomNumber } from '../helpers.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-const task = [randomNumber(), randomNumber(), randomNumber()];
-const rightAnswer = [isEven(task[0]) ? 'yes' : 'no', isEven(task[1]) ? 'yes' : 'no', isEven(task[2]) ? 'yes' : 'no'];
+/*const task = [randomNumber(), randomNumber(), randomNumber()];
+const rightAnswer = [isEven(task[0]) ? 'yes' : 'no', isEven(task[1]) ? 'yes' : 'no', isEven(task[2]) ? 'yes' : 'no'];*/
+
+const generateRound = () => {
+  const pair = [];
+  pair.push(randomNumber());
+  pair.push(isEven(pair[0]) ? 'yes' : 'no');
+
+  return pair;
+};
 
 const evenGame = () => {
-  gameLogic(description, task, rightAnswer);
+  gameLogic(description, generateRound);
 };
 
 export default evenGame;
