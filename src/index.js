696831f8459d787fe330ab10dc057f1f1346b1b9
Kirill <faeeeee@gmail.com>
2023-08-25 12:52:09 +0500

    rework general game logic

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import readlineSync from 'readline-sync';
 
-const gameLogic = (rules, task, rightAnswer) => {
+const gameLogic = (description, task, rightAnswer) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 
-  console.log(rules);
+  console.log(description);
 
-  for (let i = 0; i <= 2; i += 1) {
+  const numberOfRounds = 2;
+
+  for (let i = 0; i <= numberOfRounds; i += 1) {
     const answer = readlineSync.question(`Question: ${task[i]}\nYour answer: `);
 
-    if (answer === rightAnswer[i]) {
-      console.log('Correct!');
-    } else {
+    if (answer !== rightAnswer[i]) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer[i]}'.`);
       return console.log(`Let's try again, ${userName}!`);
     }
+    console.log('Correct!');
   }
 
   return console.log(`Congratulations, ${userName}!`);
